09bd6f2f2b406e80afbc4152f80e5e2e65d15a83
Danar Widi <widiu7omo@gmail.com>
2023-05-10 09:48:17 +0800

    Add vite inspect to autocomplete css prebuild

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import react from "@vitejs/plugin-react-swc";
 import ssr from "vite-plugin-ssr/plugin";
 import { UserConfig } from "vite";
 import UnoCSS from "unocss/vite";
+import Inspect from "vite-plugin-inspect";
+
 const config: UserConfig = {
-  plugins: [UnoCSS(), react(), ssr()],
+  plugins: [
+    UnoCSS(),
+    react(),
+    ssr(),
+    Inspect({ build: true, outputDir: ".vite-inspect" }),
+  ],
   resolve: {
     alias: {
       "#app": __dirname,
